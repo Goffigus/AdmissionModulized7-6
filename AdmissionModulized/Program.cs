c6013ffa9be300bd8f96c1835fcdd0bc61c6d5af
Goffigus <56737195+Goffigus@users.noreply.github.com>
2019-11-25 11:10:35 -0500

    Add project files.

---
+++ AdmissionModulized/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using static System.Console;
+class Admission
+{
+    static void Main()
+    {
+        double gpa;
+        int testScore;
+        string results;
+      
+        string inputString;
+        WriteLine("Enter grade point average ");
+        inputString = ReadLine();
+        gpa = Convert.ToDouble(inputString);
+        WriteLine("Enter test score ");
+        inputString = ReadLine();
+        testScore = Convert.ToInt32(inputString);
+
+        results = AcceptOrReject(gpa, testScore);
+
+        WriteLine(results);
+    }
+    public static string AcceptOrReject(double gpa, int testScore)
+    {
+        const double MINGPA = 3.0;
+        const int ADMIN1 = 60, ADMIN2 = 80;
+        string results;
+
+        if (gpa >= MINGPA)
+            if (testScore >= ADMIN1)
+                results = "Accept";
+            else
+                results = "Reject";
+        else
+           if (testScore >= ADMIN2)
+            results = "Accept";
+        else
+            results = "Reject";
+
+        return results;
+    }
+}
